394d24ee0a1218f1e8ecfb12ab3c73d8cf51b415
hamzashafiq916 <hamzashafiq916@gmail.com>
2024-06-28 22:23:09 +0500

    first commit

---
+++ src/Hamza.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Getname = () => {
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+        alias consectetur recusandae repudiandae repellendus doloremque cum quam
+        nihil, asperiores eligendi, maiores natus, porro voluptate odio?
+        Voluptatibus blanditiis ratione minima dolore!
+      </p>
+      <img src="/vite.svg" alt="" />
+    </div>
+  );
+};
+export default Getname;
